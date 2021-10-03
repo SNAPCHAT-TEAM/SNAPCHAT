6823cbac615ff576633f5bdd062cd289660043ea
SNAPCHAT-TEAM <91803381+SNAPCHAT-TEAM@users.noreply.github.com>
2021-10-03 05:10:26 +0300

    Update SNAPCHAT.lua

--- SNAPCHAT.lua
+++ SNAPCHAT.lua
@@ -4019,7 +4019,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevSNAPCHATW(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/SNAPCHAT-TEAM/Files_SNAPCHAT/main/getfile1.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/SNAPCHAT-TEAM/Files_SNAPCHAT/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
